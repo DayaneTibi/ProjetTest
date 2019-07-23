3f01f7ced72d908307786a2e09208ef3cafc7c7e
Dayane Tibi <tibi.dayane@gmail.com>
2019-07-23 09:39:23 +0200

    ajout de commentaires

--- labo5.m
+++ labo5.m
@@ -1,9 +1,9 @@
-T = 2;
+% Version comment hh
+T= 2;
 tm = 1.5; % Temps mort
 K = 2*pi/9; % Gain du régulateur
-BO = tf(K,[T 0]);
+BO = tf(K,[T 0]);  % Implmentation de la boucle ouverte (Transf de fourier)
 BO.outputd = tm;
 %margin(BO)
 nyquist(BO);
 disp("modif");
-
